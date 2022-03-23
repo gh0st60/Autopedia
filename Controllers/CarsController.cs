dfa87826e6efd3c0294dbad864b16c2c08915f37
gh0st60 <b.lazarov289@gmail.com>
2022-03-23 22:33:15 +0200

    2

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -17,9 +17,17 @@ namespace Autopedia.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pg = 1)
         {
-            List<Car> cars = _context.Cars.ToList();
+            const int pageSize = 3;
+            if (pg < 1)
+                 pg = 1;
+            int recsCount = _context.Cars.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+
+            List<Car> cars = _context.Cars.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
             return View(cars);
         }
     }
